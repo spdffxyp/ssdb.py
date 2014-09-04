7be5382a722ad35d8be40fb4efee6493adb9a2d0
hit9 <nz2324@126.com>
2014-09-05 01:08:14 +0800

    upgrade setup

--- setup.py
+++ setup.py
@@ -4,21 +4,6 @@ ssdb.py
 
 Ssdb Python Client Library.
 
-Sample
-``````
-
-.. code:: python
-
-    >>> from ssdb import SSDBClient
-    >>> c = SSDBClient(host='localhost', port=8888)
-    >>> c.set('key', 'val')
-    1
-    >>> c.get('key')
-    'val'
-
-Source
-``````
-
 https://github.com/hit9/ssdb.py
 """
 
@@ -27,7 +12,7 @@ from setuptools import setup
 
 setup(
     name='ssdb.py',
-    version='0.1.4',
+    version='0.1.5',
     author='hit9',
     author_email='nz2324@126.com',
     description="Ssdb Python Client Library",
@@ -37,12 +22,15 @@ setup(
     url='https://github.com/hit9/ssdb.py',
     py_modules=['ssdb'],
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
 )
